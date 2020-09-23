b69d27f4ff68c4564a5443dc6283e00fbdb55325
arcantu97 <arcantu97@gmail.com>
2020-09-22 20:36:07 -0500

    Redactando resultados del primer experimento

--- scripts/piechart.py
+++ scripts/piechart.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 labels = 'Pino', 'Abies', 'Encino'
 sizes = [35273, 7544, 9533]
 explode = (0,0,0)
-
+colors=['#004de5', '#26e600', '#fee600']
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
