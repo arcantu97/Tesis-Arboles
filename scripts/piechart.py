a91e498baa59c3de782e846c4f538f4a4b5f3e43
arcantu97 <arcantu97@gmail.com>
2020-09-29 23:54:38 -0500

    Agregando otros experimentos y figuras high-res

--- scripts/piechart.py
+++ scripts/piechart.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt 
 
 labels = 'Pino', 'Abies', 'Encino'
-sizes = [34439, 7497, 10414]
+sizes = [67.4, 14.4, 18.4]
 explode = (0,0,0)
 colors=['#004de5', '#26e600', '#fee600']
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
-        shadow=True, startangle=90)
+ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-plt.savefig('results_2.png', dpi=None, facecolor='w', edgecolor='w',
-        orientation='portrait', papertype=None, format=None,
+plt.savefig('results.eps', dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format='eps',
         transparent=True, bbox_inches=None, pad_inches=0.1,
         frameon=None, metadata=None)
