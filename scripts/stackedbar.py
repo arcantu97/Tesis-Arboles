7f1bfb9640f12b09ea5568d15935ec9d6dbb1a1f
arcantu97 <arcantu97@gmail.com>
2020-10-13 21:16:58 -0500

    Describiendo resultados de experimentos y discusión de objetivos logrados

--- scripts/stackedbar.py
+++ scripts/stackedbar.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-X_AXIS = ('0.15', '0.25', '0.50')
+X_AXIS = ('0.75', '0.80', '0.85')
 index = pd.Index(X_AXIS, name='Porcentaje de píxeles permitidos')
-data = {'Pino': (66.0, 62.0, 67.0),
-'Encino': (17.0, 24.0, 15.0), 
-'Abies': (17.0, 14.0, 18.0)}
+data = {'Pino': (70.0, 63.0, 66.0),
+'Encino': (20.0, 25.0, 22.0), 
+'Abies': (10.0, 12.0, 12.0)}
 df = pd.DataFrame(data, index=index)
 ax = df.plot(kind='bar', stacked=True, figsize=(10, 6))
 ax.set_ylabel('foo')
 plt.legend(title='labels', bbox_to_anchor=(1.0, 1), loc='upper left')
-plt.savefig('stacked.png')  # if needed
+plt.savefig('stacked.eps')  # if needed
 #plt.show()
